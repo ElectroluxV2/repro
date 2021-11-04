95d8039af66b686dc316ebc71806b64b8946696c
electroluxv2 <electroluxv2@gmail.com>
2021-11-04 14:24:41 +0100

    minimal repro

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        SearchParameters parameters =new SearchParameters();
-        parameters.setAgeFrom(20);
-        parameters.setAgeTo(40);
-        parameters.setIncomeFrom(2000);
-        parameters.setPage(new Page(9,1));
-        parameters.getSelectedGenders().add(Gender.FEMALE);
-        parameters.getSelectedGenders().add(Gender.OTHER);
-        parameters.getFunctions().add(new FunctionsParameters("age", Funcs.AVARAGE));
-        parameters.getFunctions().add(new FunctionsParameters("income", Funcs.SUM));
-        parameters.getFunctions().add(new FunctionsParameters("income", Funcs.AVARAGE));
+        SearchParameters parameters = new SearchParameters();
+        parameters.getFunctions().add(new FunctionsParameters("age"));
 
         final var items = People.Data;
 
@@ -18,21 +10,14 @@ public class Main {
                 .map(functionsParameters -> {
                     final var fieldValuesStream = items.stream()
                             .map(person -> PersonFieldGetter.getFieldValueAsNumber(functionsParameters.getFieldName(), person))
-                            .mapToDouble(value -> value.doubleValue());
+                            .mapToDouble(Number::doubleValue);
                     // When .mapToDouble(Number::doubleValue); used:
                     // java: incompatible types: invalid method reference
                     //    method doubleValue in class java.lang.Number cannot be applied to given types
                     //      required: no arguments
                     //      found:    java.lang.Object
                     //      reason: actual and formal argument lists differ in length
-                    return new FunctionResult(
-                            functionsParameters.getFunction(),
-                            functionsParameters.getFieldName(),
-                            switch (functionsParameters.getFunction()) {
-                                case SUM -> fieldValuesStream.sum();
-                                case AVARAGE -> fieldValuesStream.average().orElse(0);
-                            }
-                    );
+                    return null;
                 })
                 .toList();
     }
--- src/People.java
+++ src/People.java
@@ -2,25 +2,25 @@ import java.util.List;
 
 public class People {
     public final static Person JanKowalski =
-            new Person("jan", "kowalski", 30, 1200, Gender.MALE);
+            new Person(30, 1200);
     public final static Person MateuszNowak =
-            new Person("mateusz", "nowak", 56, 3000, Gender.MALE);
+            new Person(56, 3000);
     public final static Person AnnaBuda =
-            new Person("anna", "buda", 22, 4500, Gender.FEMALE);
+            new Person( 22, 4500);
     public final static Person ConchitaWurst =
-            new Person("conchita", "wurst", 36, 12000, Gender.OTHER);
+            new Person(36, 12000);
     public final static Person MariaKowalewicz=
-            new Person("maria", "kowalewicz", 46, 1500, Gender.FEMALE);
+            new Person(46, 1500);
     public final static Person AnetaUrban=
-            new Person("aneta", "urban", 21, 4200, Gender.FEMALE);
+            new Person( 21, 4200);
     public final static Person ZdzislawSzydlowski=
-            new Person("zdzislaw", "szydlowski", 38, 6800, Gender.MALE);
+            new Person(38, 6800);
     public final static Person JanAnrusowski=
-            new Person("jan", "anrusowki", 49, 1250, Gender.MALE);
+            new Person( 49, 1250);
     public final static Person JanuszKowalski=
-            new Person("janusz", "kowalski", 18, 1000, Gender.MALE);
+            new Person(18, 1000);
     public final static Person DanutaKowalska=
-            new Person("danuta", "kowalska", 27, 1200, Gender.FEMALE);
+            new Person(27, 1200);
 
     public static List<Person> Data = List.of(
             JanKowalski,
